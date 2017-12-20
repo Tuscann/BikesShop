b188156db9f8b38108b929823622afdf0ce1b653
Tuscann <fbinnzhivko@gmail.com>
2017-12-20 23:00:38 +0200

    Bike shop

---
+++ js/empty.js
@@ -0,0 +1,16 @@
+
+carousel();
+
+function carousel() {
+    let myIndex = 0;
+    let x = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "initial";
+    setTimeout(carousel, 4000);
+}
