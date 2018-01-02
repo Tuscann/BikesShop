b26942ddfb783ffbcdbaede734c27620ede1fbf7
Tuscann <fbinnzhivko@gmail.com>
2018-01-02 11:49:44 +0200

    Add galery

--- js/empty.js
+++ js/empty.js
@@ -4,13 +4,10 @@ carousel();
 function carousel() {
     let myIndex = 0;
     let x = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
     myIndex++;
     if (myIndex > x.length) {
         myIndex = 1
     }
-    x[myIndex - 1].style.display = "initial";
+
     setTimeout(carousel, 4000);
 }
