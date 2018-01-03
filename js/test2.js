103c2d6968417c9c32817cbac30b0236acf72446
Tuscann <fbinnzhivko@gmail.com>
2018-01-03 19:13:54 +0200

    final

---
+++ js/test2.js
@@ -0,0 +1,38 @@
+filterSelection("all");        // Execute the function and show all columns
+function filterSelection(c) {
+    let x = document.getElementsByClassName("column");
+    if (c == "all"){
+        c = "";
+    }
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (let i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) {
+            AddClass(x[i], "show");
+        }
+    }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) === -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
